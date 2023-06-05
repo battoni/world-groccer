06f137ed173e095cf9f1d8d24d7ef582024bf368
Guilherme Battoni <guilherme.battoni@gmail.com>
2023-06-05 13:54:26 -0300

    chore: Add components folder

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
+import Libraries from '@Libraries';
 import Plugins from '@Plugins';
 import App from './App.vue';
 
 const app = createApp(App);
 
+Libraries.forEach((lib) => app.use(lib));
 Plugins.forEach((plugin) => app.use(plugin));
 
 app.mount('#app');
