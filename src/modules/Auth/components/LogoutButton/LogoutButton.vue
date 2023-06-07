c44934a19f6cab5eb942dbdc01d357aa6b357fd5
Guilherme Battoni <guilherme.battoni@gmail.com>
2023-06-07 19:22:23 -0300

    feat(Modules): add auth module

---
+++ src/modules/Auth/components/LogoutButton/LogoutButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { BaseIcon } from '@BaseUi';
+import { APP_ICONS } from '@Helpers';
+</script>
+
+<template>
+  <div class="logout">
+    <BaseIcon
+      isButton
+      :icon="APP_ICONS.faDoorOpen"
+    />
+
+    <AppText>Log out</AppText>
+  </div>
+</template>
+
+<style scoped>
+.logout {
+  @apply flex gap-2 items-center text-text-active;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@Assets': fileURLToPath(new URL('./src/assets/', import.meta.url)),
-      '@AppUi': fileURLToPath(new URL('./src/components/app', import.meta.url)),
-      '@BaseUi': fileURLToPath(
-        new URL('./src/components/base', import.meta.url)
-      ),
       '@Composables': fileURLToPath(
         new URL('./src/composables', import.meta.url)
       ),
@@ -24,6 +20,18 @@ export default defineConfig({
       '@Styles': fileURLToPath(new URL('./src/styles/', import.meta.url)),
       '@Types': fileURLToPath(new URL('./src/types', import.meta.url)),
       '@Views': fileURLToPath(new URL('./src/views', import.meta.url)),
+
+      '@AppUi': fileURLToPath(new URL('./src/components/app', import.meta.url)),
+      '@BaseUi': fileURLToPath(
+        new URL('./src/components/base', import.meta.url)
+      ),
+      '@ComposedUi': fileURLToPath(
+        new URL('./src/components/composed', import.meta.url)
+      ),
+
+      '@AuthModule': fileURLToPath(
+        new URL('./src/modules/Auth', import.meta.url)
+      ),
     },
   },
   test: {
