e8edbb75ca61bc91e076c9b69b23f2cde5e03635
Guilherme Battoni <guilherme.battoni@gmail.com>
2023-06-08 13:30:09 -0300

    feat(HomeView): Add new sections

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
 import { TheDashboardLayout } from '@Layouts';
-import { KpiHighlight } from '@OfferModule';
+import { KpiClvTrending, KpiHighlight, KpiStatistics } from '@OfferModule';
 </script>
 
 <template>
   <TheDashboardLayout>
-    <KpiHighlight />
+    <section class="highlights-section">
+      <KpiHighlight />
+    </section>
+
+    <section class="statistics-section">
+      <KpiStatistics />
+
+      <KpiClvTrending />
+    </section>
   </TheDashboardLayout>
 </template>
+
+<style scoped>
+.highlights-section {
+  @apply mb-5;
+}
+.statistics-section {
+  @apply flex gap-5 justify-start;
+}
+</style>
